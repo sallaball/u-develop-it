3127e71f994c559585bea35337f17545106a1093
sallaball <sallaball1206@gmail.com>
2022-08-15 14:17:07 +0300

    connected to database

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'coding',
+        database: 'election'
+    },
+    console.log("Connected to the election database.")
+);
+
 
 //Default response for any other request (Not Found)
-app.use((req,res) => {
+app.use((req, res) => {
     res.status(404).end();
 });
 
