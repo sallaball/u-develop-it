93fe32b86044f76e27971c9f438a03f8beff3712
sallaball <sallaball1206@gmail.com>
2022-08-15 16:42:35 +0300

    get data testing

--- server.js
+++ server.js
@@ -19,6 +19,22 @@ const db = mysql.createConnection(
     console.log("Connected to the election database.")
 );
 
+//Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 // db.query(`SELECT * FROM candidates`, (err, rows) => {
 //     console.log(rows);
 // });
@@ -40,16 +56,16 @@ const db = mysql.createConnection(
 // });
 
 //Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-            const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+//             const params = [1, 'Ronald', 'Firbank', 1];
 
-            db.query(sql, params, (err, result) => {
-                if (err) {
-                    console.log(err);
-                }
-                console.log(result);
-            });
+//             db.query(sql, params, (err, result) => {
+//                 if (err) {
+//                     console.log(err);
+//                 }
+//                 console.log(result);
+//             });
 
 
 //Default response for any other request (Not Found)
