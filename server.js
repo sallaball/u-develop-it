8f67f72a726f9ea8921f671084a3e5a3869f6295
sallaball <sallaball1206@gmail.com>
2022-08-15 16:50:31 +0300

    added get all and get one candidate

--- server.js
+++ server.js
@@ -40,12 +40,21 @@ app.get('/api/candidates', (req, res) => {
 // });
 
 //GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1 `, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// })
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+db.query(sql,params, (err, row) => {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+   res.json({
+    message: 'success',
+    data: row
+   });
+});
+});
 
 //Delete a candidate
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
